9619a69788ef2fdbff364f9e2d2cafe6947a305e
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-31 22:05:20 -0700

    add landing page/ change screen shots

---
+++ landingPage/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	var factor = -1;
+
+ $(".startingStage").click(function(){
+               $('.myphoto').animate( {width: 400, height: 400}, 1000 );
+               $('.mylogo').animate( {width: 250, height: 'auto'}, 1000 );
+               $('.logo').animate( {width: 500, height: 70 }, 1000 );
+            });
+
+
+});
