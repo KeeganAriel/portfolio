298c88cd7e4a44550e9c6510f3f6f2bddbdf5e16
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-31 22:31:04 -0700

    add animate to loading page

--- landingPage/app.js
+++ landingPage/app.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 	var factor = -1;
 
  $(".startingStage").click(function(){
-               $('.myphoto').animate( {width: 400, height: 400}, 1000 );
-               $('.mylogo').animate( {width: 250, height: 'auto'}, 1000 );
-               $('.logo').animate( {width: 500, height: 70 }, 1000 );
+               $('.myphoto').animate( {width: 350, height: 350, left: -275}, 1000 );
+               $('.mylogo').animate( {width: 250, height: 'auto', left: -10}, 1000 );
+               $('.logo').animate( {width: 400, height: 65, left: -275, top: -30 }, 1000 );
             });
 
 
