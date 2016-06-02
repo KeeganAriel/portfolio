f404deeca9487cf4d2c1e2b84f38c472b6b3a933
KeeganAriel <keegan.teinowitz@gmail.com>
2016-06-01 22:28:27 -0700

    delete

--- landingPage/notes.js
+++
@@ -1,9 +0,0 @@
-    $(this).animate({
-        top: '-=' + $(this).height() / factor,
-        left: '-=' + $(this).width() / factor,
-        width: $(this).width() * factor
-    });
-
-    $('.myphoto').click(function() {
-	$(this).animate({'height': '100px'}, 100);
-});
