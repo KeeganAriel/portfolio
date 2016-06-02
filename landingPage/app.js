b0405125e52f48b9394aa3f01f9b0cd6063cc4f3
KeeganAriel <keegan.teinowitz@gmail.com>
2016-06-02 10:35:54 -0700

    add images

--- landingPage/app.js
+++ landingPage/app.js
@@ -6,11 +6,12 @@ $(document).ready(function () {
 
         $('.headtext1').animate( { "opacity": "show", top:"100"} , 1500 );
 
-               $('.myphoto').animate( {width: 380, height: 380, left: -278, top: 13}, 1500 );
+               $('.myphoto').animate( {width: 450, height: 450, left: -278, top: 13}, 1500 );
                   $('.mylogo').animate({"opacity": 1, width: 400, height: 'auto', left: 575, top: 160}, 1500);
                     $('.logo').animate( { width: 0, 'margin': 0, }, 1000); 
                     $('.strengths').slideDown(1500); 
                     $('.heart').slideDown(1300); 
+                    $('.design').slideDown(1300);
             });
 
 
